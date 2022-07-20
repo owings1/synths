d9c0e4fef7cae151e6e955c2ade2a95477025be5
Doug Owings <doug@dougowings.net>
2022-07-20 08:06:54 -0700

    effects

--- js/demo1.js
+++ js/demo1.js
@@ -9,60 +9,53 @@ import $ from './jquery.js'
 import * as Music from './music.js'
 import * as Effects from './effects.js'
 
-const Ctx = new AudioContext()
-
-// Main volume.
-const Main = Ctx.createGain()
+const Context = new AudioContext()
 
 // Oscillator.
-const Oscillator = Ctx.createOscillator()
-Oscillator.frequency.value = Music.stepFreq(440)
+const Oscillator = new OscillatorNode(Context, {frequency: Music.stepFreq(440)})
+
+// Main volume.
+const Main = new GainNode(Context)
 // Oscillator dry signal.
-Oscillator.dry = Ctx.createGain()
-Oscillator.connect(Oscillator.dry)
-Oscillator.dry.connect(Main)
+const Dry = new GainNode(Context)
 // Oscillator FX send
-Oscillator.fxsend = Ctx.createGain()
-Oscillator.connect(Oscillator.fxsend)
+const FxSend = new GainNode(Context)
 
-// Distortion
-const Distortion = Ctx.createWaveShaper()
-Distortion.oversample = '4x'
-Distortion.curve = Effects.makeDistortionCurve(400)
-Distortion.gain = Ctx.createGain()
-Distortion.gain.gain.value = 1.2
-Distortion.connect(Distortion.gain)
+Oscillator.connect(Dry)
+Oscillator.connect(FxSend)
+Dry.connect(Main)
 
-const Overdrive = new Effects.Overdrive(Ctx)
-// Delay
-const Delay = Ctx.createDelay()
-// Delay feedback
-Delay.feedback = Ctx.createGain()
-Delay.connect(Delay.feedback)
-Delay.feedback.connect(Delay)
+const Distortion = new Effects.Distortion(Context)
+const Overdrive = new Effects.Overdrive(Context)
+const Delay = new Effects.Delay(Context)
+const Lowpass = new Effects.Lowpass(Context)
+const Highpass = new Effects.Highpass(Context)
+const Compressor = new Effects.Compressor(Context)
 
-// Lowpass Filter
-const Lowpass = Ctx.createBiquadFilter({type: 'lowpass'})
-
-// Highpass Filter
-const Highpass = Ctx.createBiquadFilter({type: 'highpass'})
-
-// Compressor
-const Compressor = Ctx.createDynamicsCompressor()
+// FX Chain
+Effects.initChain(FxSend, Main, [
+    Distortion,
+    Overdrive,
+    Delay,
+    Lowpass,
+    Highpass,
+    Compressor,
+])
 
 // Element IDs to parameter.
 const ParamValueSetters = {
     'volume': Main.gain,
-    'oscillator-dry': Oscillator.dry.gain,
-    'oscillator-fxsend': Oscillator.fxsend.gain,
+    'oscillator-dry': Dry.gain,
+    'oscillator-fxsend': FxSend.gain,
     'oscillator-frequency': Oscillator.frequency,
     'overdrive-gain': Overdrive.gain,
     'overdrive-drive': Overdrive.drive,
     'overdrive-color': Overdrive.color,
     'overdrive-preband': Overdrive.preBand,
     'overdrive-postcut': Overdrive.postCut,
+    'delay-gain': Delay.gain,
     'delay-time': Delay.delayTime,
-    'delay-feedback': Delay.feedback.gain,
+    'delay-feedback': Delay.feedback,
     'lowpass-frequency': Lowpass.frequency,
     'lowpass-quality': Lowpass.Q,
     'highpass-frequency': Highpass.frequency,
@@ -74,15 +67,9 @@ const ParamValueSetters = {
     'compressor-release': Compressor.release,
 }
 
-const GenericGetSet = {
-    // 'overdrive-preband': {
-    //     get: () => Overdrive.preBand,
-    //     set: value => Overdrive.preBand = value,
-    // },
-}
 // Element IDs to node.
 const NodeActiveCheckboxes = {
-    'distortion-active': Distortion.gain,
+    'distortion-active': Distortion,
     'overdrive-active': Overdrive,
     'delay-active': Delay,
     'lowpass-active': Lowpass,
@@ -90,76 +77,13 @@ const NodeActiveCheckboxes = {
     'compressor-active': Compressor,
 }
 
-// FX Chain
-
-const FxIn = Oscillator.fxsend
-const FxOut = Main
-
-FxIn.receiver = FxIn
-FxOut.receiver = FxOut
-
-const FxChain = [
-    Distortion.gain,
-    Overdrive,
-    Delay,
-    Lowpass,
-    Highpass,
-    Compressor,
-]
-
-FxChain.forEach((node, i, arr) => {
- 
-    node.prev = arr[i - 1] || FxIn
-    node.next = arr[i + 1] || FxOut
-    node.bypass = node.receiver = Ctx.createGain()
-    let active = false
- 
-    Object.defineProperty(node, 'active', {
-        get: () => active,
-        set: function(value) {
-            value = Boolean(value)
-            if (value === active) {
-                return
-            }
-            active = value
-            const {prev, next} = this
-            // Disconnect
-            if (prev) {
-                prev.receiver.disconnect(this.receiver)
-            }
-            if (next) {
-                this.receiver.disconnect(next.receiver)
-            }
-            // Change receiver
-            this.receiver = active ? this : this.bypass
-            // Reconnect
-            if (prev) {
-                prev.receiver.connect(this.receiver)
-            }
-            if (next) {
-                this.receiver.connect(next.receiver)
-            }
-        },
-    })
-})
-
-// Initialize FX connections.
-FxChain.forEach(node => {
-    if (node.prev) {
-        node.prev.receiver.connect(node.receiver)
-    }
-    if (node.next) {
-        node.receiver.connect(node.next.receiver)
-    }
-})
-
 $(document).on('click', function(e) {
     const $target = $(e.target)
     const id = $target.attr('id')
     switch (id) {
         case 'start':
             $('#stop').prop('disabled', false)
-            Main.connect(Ctx.destination)
+            Main.connect(Context.destination)
         case 'stop':
             Oscillator[id]()
             $target.prop('disabled', true)
@@ -192,12 +116,6 @@ $(document).on('click', function(e) {
         updateMeters()
         return
     }
-    // let {set} = GenericGetSet[id]
-    // if (set) {
-    //     set(value)
-    //     updateMeters()
-    //     return
-    // }
     let name = $target.attr('name')
     switch (name) {
         case 'oscillator-type':
@@ -220,15 +138,12 @@ function updateMeters() {
     Object.entries(ParamValueSetters).forEach(([id, param]) => {
         const {value} = param
         if (value !== undefined) {
-            $(`#${id}-meter`).text(value.toFixed(2))
+            const $el = $(`#${id}-meter`)
+            if ($el.length) {
+                $el.text(value.toFixed(2))
+            }
         }
     })
-    // Object.entries(GenericGetSet).forEach(([id, {get}]) => {
-    //     const value = get()
-    //     if (value !== undefined) {
-    //         $(`#${id}-meter`).text(value.toFixed(2))
-    //     }
-    // })
 }
  
 /**
@@ -241,12 +156,6 @@ function readParams() {
             param.value = $el.val()
         }
     })
-    // Object.entries(GenericGetSet).forEach(([id, {set}]) => {
-    //     const $el = $(`#${id}`)
-    //     if ($el.length) {
-    //         set($el.val())
-    //     }
-    // })
     Object.entries(NodeActiveCheckboxes).forEach(([id, node]) => {
         const $el = $(`#${id}`)
         if ($el.length) {
--- js/effects.js
+++ js/effects.js
@@ -1,6 +1,202 @@
+/**
+ * WebAudio effects.
+ * 
+ * @author Doug Owings <doug@dougowings.net>
+ */
 
 /**
- * Overdrive effect module for the Web Audio API.
+ * Compressor.
+ */
+export class Compressor extends DynamicsCompressorNode {
+    /**
+     * @param {AudioContext} context
+     * @param {object} opts
+     */
+    constructor(context, opts) {
+        super(context, opts)
+    }
+    get meta() { return this.constructor.Meta }
+}
+
+Compressor.Meta = {
+    name: 'Compressor',
+    params: {
+        // TODO
+    },
+}
+
+/**
+ * Basic lowpass filter
+ */
+export class Lowpass extends BiquadFilterNode {
+    /**
+     * @param {AudioContext} context
+     * @param {object} opts
+     */
+    constructor(context, opts) {
+        super(context, opts)
+        this.type = 'lowpass'
+    }
+    get meta() { return this.constructor.Meta }
+}
+
+Lowpass.Meta = {
+    name: 'Lowpass',
+    params: {
+        // TODO
+    },
+}
+
+/**
+ * Basic highpass filter.
+ */
+export class Highpass extends BiquadFilterNode {
+    /**
+     * @param {AudioContext} context
+     * @param {object} opts
+     */
+    constructor(context, opts) {
+        super(context, opts)
+        this.type = 'highpass'
+    }
+    get meta() { return this.constructor.Meta }
+}
+
+Highpass.Meta = {
+    name: 'Highpass',
+    params: {
+        // TODO
+    },
+}
+
+/**
+ * Delay with feedback.
+ */
+export class Delay extends GainNode {
+
+    /**
+     * @param {AudioContext} context
+     * @param {object} opts
+     * @param {Number} opts.delayTime
+     * @param {Number} opts.feedback
+     */
+    constructor(context, opts = {}) {
+        super(context)
+        opts = optsMerge(this.meta.params, opts)
+
+        const dy = new DelayNode(context)
+        const fb = new GainNode(context)
+
+        Object.defineProperties(this, {
+            feedback: {value: fb.gain},
+            delayTime: {value: dy.delayTime},
+        })
+
+        this.output = new GainNode(context)
+
+        super.connect(dy)
+        dy.connect(fb)
+        fb.connect(dy)
+        dy.connect(this.output)
+
+        this.gain.value = opts.gain
+        this.feedback.value = opts.feedback
+        this.delayTime.value = opts.delayTime
+    }
+
+    /** @param {AudioNode} dest */
+    connect(dest) { return this.output.connect(dest) }
+    disconnect(...args) { this.output.disconnect(...args)}
+    get meta() { return this.constructor.Meta }
+}
+
+Delay.Meta = {
+    name: 'Delay',
+    params: {
+        gain: {
+            min: 0.0,
+            max: 3.0,
+            default: 1.0,
+            type: "float",
+        },
+        feedback: {
+            min: 0.0,
+            max: 1.0,
+            default: 0.5,
+            type: "float",
+        },
+        delayTime: {
+            min: 0.0,
+            max: 1.0,
+            default: 0.5,
+            type: "float",
+        },
+    }
+}
+
+/**
+ * Basic distortion effect.
+ */
+export class Distortion extends GainNode {
+
+    /**
+     * @param {AudioContext} context
+     * @param {object} opts
+     * @param {Number} opts.gain
+     * @param {Number} opts.drive
+     */
+    constructor(context, opts = {}) {
+        super(context)
+        opts = optsMerge(this.meta.params, opts)
+        
+        const ws = new WaveShaperNode(context)
+
+        let drive
+
+        Object.defineProperties(this, {
+            drive: paramProp(() => drive, value => {
+                drive = Number(value)
+                ws.curve = makeDistortionCurve(drive * 1000)
+            }),
+        })
+
+        this.output = new GainNode(context)
+
+        super.connect(ws)
+        ws.connect(this.output)
+        
+        ws.oversample = '4x'
+
+        this.drive.value = opts.drive
+        this.gain.value = opts.gain
+    }
+
+    /** @param {AudioNode} dest */
+    connect(dest) { return this.output.connect(dest) }
+    disconnect(...args) { this.output.disconnect(...args)}
+    get meta() { return this.constructor.Meta }
+}
+
+Distortion.Meta = {
+    name: "Distortion",
+    params: {
+        gain: {
+            min: 1.0,
+            max: 3.0,
+            default: 1.2,
+            type: "float",
+        },
+        drive: {
+            min: 0.0,
+            max: 1.0,
+            default: 0.4,
+            type: "float",
+        },
+    }
+}
+
+/**
+ * Overdrive effect.
  * 
  * @see https://github.com/web-audio-components/overdrive
  * 
@@ -24,182 +220,211 @@ export class Overdrive extends GainNode {
     /**
      * @param {AudioContext} context
      * @param {object} opts
-     * @param {number} opts.preBand
-     * @param {number} opts.color
-     * @param {number} opts.drive
-     * @param {number} opts.postCut
+     * @param {Number} opts.preBand
+     * @param {Number} opts.color
+     * @param {Number} opts.drive
+     * @param {Number} opts.postCut
      */
-    constructor(ctx, opts) {
-        super(ctx)
-        this.output = ctx.createGain()
-      
-        // Internal AudioNodes
-        this._bandpass = ctx.createBiquadFilter()
-        this._bpWet = ctx.createGain()
-        this._bpDry = ctx.createGain()
-        this._ws = ctx.createWaveShaper()
-        this._lowpass = ctx.createBiquadFilter()
-      
-        // AudioNode graph routing
-        super.connect(this._bandpass) // input node
-        this._bandpass.connect(this._bpWet)
-        this._bandpass.connect(this._bpDry)
-        this._bpWet.connect(this._ws)
-        this._bpDry.connect(this._ws)
-        this._ws.connect(this._lowpass)
-        this._lowpass.connect(this.output)
-      
+    constructor(context, opts = {}) {
+        super(context)
+        opts = optsMerge(this.meta.params, opts)
 
-        const that = this
-        this.color = {
-            get value() { return that._bandpass.frequency.value },
-            set value(value) {
-                // that._bandpass.frequency.value = value
-                that._bandpass.frequency.setValueAtTime(value, 0)
-            },
-        }
-        this.preBand = {
-            get value() { return that._bpWet.gain.value },
-            set value(value) {
+        const bp = new BiquadFilterNode(context)
+        const bpWet = new GainNode(context)
+        const bpDry = new GainNode(context)
+        const ws = new WaveShaperNode(context)
+        const lp = new BiquadFilterNode(context)
+
+        let drive
+
+        Object.defineProperties(this, {
+            color: paramProp(() => bp.frequency.value, value => {
+                bp.frequency.setValueAtTime(value, 0)
+            }),
+            preBand: paramProp(() => bpWet.gain.value, value => {
                 value = Number(value)
-                that._bpWet.gain.setValueAtTime(value, 0)
-                that._bpDry.gain.setValueAtTime(1 - value, 0)
-            }
-        }
-        this.drive = {
-            get value() { return that._drive },
-            set value(value) {
-                value = Number(value)
-                that._drive = value
-                that._ws.curve = makeDistortionCurve(value * 100, 22050)
-            }
-        }
-        this.postCut = {
-            get value() { return that._lowpass.frequency.value },
-            set value(value) {
-                that._lowpass.frequency.setValueAtTime(value, 0)
-            }
-        }
-        // Defaults
-        opts = opts || {}
-        Object.entries(this.meta.params).forEach(([name, param]) => {
-            opts[name] = opts[name] || param.defaultValue
+                bpWet.gain.setValueAtTime(value, 0)
+                bpDry.gain.setValueAtTime(1 - value, 0)
+            }),
+            drive: paramProp(() => drive, value => {
+                drive = Number(value)
+                ws.curve = makeDistortionCurve(drive * 100, 22050)
+            }),
+            postCut: paramProp(() => lp.frequency.value, value => {
+                lp.frequency.setValueAtTime(value, 0)
+            }),
         })
-        this.gain.value = opts.gain
-        this._bandpass.frequency.value = opts.color
-        this._bpWet.gain.value = opts.preBand
-        this._lowpass.frequency.value = opts.postCut
+
+        this.output = new GainNode(context)
       
-        // Inverted preBand value
-        this._bpDry.gain.value = 1 - opts.preBand
+        super.connect(bp)
+        bp.connect(bpWet)
+        bp.connect(bpDry)
+        bpWet.connect(ws)
+        bpDry.connect(ws)
+        ws.connect(lp)
+        lp.connect(this.output)
+
+        bp.frequency.value = opts.color
+        bpWet.gain.value = opts.preBand
+        bpDry.gain.value = 1 - opts.preBand
+        lp.frequency.value = opts.postCut
+    
+        this.gain.value = opts.gain
         this.drive.value = opts.drive
     }
 
-    /**
-     * AudioNode prototype `connect` method.
-     * 
-     * @param {AudioNode} dest
-     */
-    connect(dest) {
-        return this.output.connect(dest.input ? dest.input : dest)
-    }
-
-    /**
-     * AudioNode prototype `disconnect` method.
-     */
-    disconnect() {
-        this.output.disconnect()
-    }
-
-    get meta() {
-        return Overdrive.Meta
-    }
-
-    // /** @type {float} */
-    // get preBand() {
-    //     return this._bpWet.gain.value
-    // }
-
-    // set preBand(value) {
-    //     value = Number(value)
-    //     this._bpWet.gain.setValueAtTime(value, 0)
-    //     this._bpDry.gain.setValueAtTime(1 - value, 0)
-    // }
-
-    // /** @type {float} */
-    // get color() {
-    //     return this._bandpass.frequency.value
-    // }
-
-    // set color(value) {
-    //     this._bandpass.frequency.setValueAtTime(value, 0)
-    // }
-
-    // /** @type {float} */
-    // get drive() {
-    //     return this._drive
-    // }
-
-    // set drive(value) {
-    //     value = Number(value)
-    //     this._drive = value
-    //     this._ws.curve = makeDistortionCurve(value * 100, 22050)
-    // }
-
-    // /** @type {float} */
-    // get postCut() {
-    //     return this._lowpass.frequency.value
-    // }
-
-    // set postCut(value) {
-    //     this._lowpass.frequency.setValueAtTime(value, 0)
-    // }
+    /** @param {AudioNode} dest */
+    connect(dest) { return this.output.connect(dest) }
+    disconnect(...args) { this.output.disconnect(...args)}
+    get meta() { return this.constructor.Meta }
 }
 
 Overdrive.Meta = {
     name: "Overdrive",
     params: {
+        gain: {
+            min: 1.0,
+            max: 3.0,
+            default: 2.0,
+            type: "float",
+        },
         preBand: {
             min: 0,
             max: 1.0,
-            defaultValue: 0.5,
+            default: 0.5,
             type: "float",
         },
         color: {
             min: 0,
             max: 22050,
-            defaultValue: 800,
+            default: 800,
             type: "float",
         },
         drive: {
             min: 0.0,
             max: 1.0,
-            defaultValue: 0.5,
+            default: 0.5,
             type: "float",
         },
         postCut: {
             min: 0,
             max: 22050,
-            defaultValue: 3000,
+            default: 3000,
             type: "float",
         },
-        gain: {
-            min: 1.0,
-            max: 3.0,
-            defaultValue: 2.0,
-            type: "float",
-        }
     },
 }
 
-const DEG = Math.PI / 180
+/**
+ * Build chain with `receiver`, `prev`, `next`, `active` properties.
+ * 
+ * @param {AudioNode} input The input node.
+ * @param {AudioNode} output The output node.
+ * @param {AudioNode[]} chain The effects chain.
+ */
+ export function initChain(input, output, chain) {
 
+    const {context} = input
+    input.receiver = input
+    output.receiver = output
+    
+    chain.forEach((node, i) => {
+ 
+        node.prev = chain[i - 1] || input
+        node.next = chain[i + 1] || output
+        node.bypass = node.receiver = new GainNode(context)
+        let active = false
+     
+        Object.defineProperty(node, 'active', {
+            get: () => active,
+            set: function(value) {
+                value = Boolean(value)
+                if (value === active) {
+                    return
+                }
+                active = value
+                const {prev, next} = this
+                // Disconnect
+                if (prev) {
+                    prev.receiver.disconnect(this.receiver)
+                }
+                if (next) {
+                    this.receiver.disconnect(next.receiver)
+                }
+                // Change receiver
+                this.receiver = active ? this : this.bypass
+                // Reconnect
+                if (prev) {
+                    prev.receiver.connect(this.receiver)
+                }
+                if (next) {
+                    this.receiver.connect(next.receiver)
+                }
+            },
+        })
+    })
+    
+    // Initialize FX connections.
+    chain.forEach(node => {
+        if (node.prev) {
+            node.prev.receiver.connect(node.receiver)
+        }
+        if (node.next) {
+            node.receiver.connect(node.next.receiver)
+        }
+    })
+}
+
+/**
+ * @param {Number} amount
+ * @param {integer} n
+ * @return {Float32Array}
+ */
 export function makeDistortionCurve(amount = 50, n = 44100) {
     const k = Number(amount)
     const curve = new Float32Array(n)
+    const DEG = Math.PI / 180
     for (let i = 0; i < n; i++) {
         const x = i * 2 / n - 1
         curve[i] = (3 + k) * x * 20 * DEG / (Math.PI + k + Math.abs(x))
     }
     return curve
+}
+
+/**
+ * @param {object[]} defs Param definitions with `default`.
+ * @param {*} opts User options.
+ * @return {object} Modified `opts` or new object.
+ */
+function optsMerge(defs, opts) {
+    opts = opts || {}
+    Object.entries(defs).forEach(([name, def]) => {
+        opts[name] = opts[name] || def.default
+    })
+    return opts
+}
+
+/**
+ * @param {Function} vget Setter for `value` property.
+ * @param {Function} vset Getter for `value` property.
+ * @return {object}
+ */
+function paramObject(vget, vset) {
+    return {
+        get value() { return vget() },
+        set value(value) { vset(value) },
+    }
+}
+
+/**
+ * @param {Function} vget Setter for `value` property.
+ * @param {Function} vset Getter for `value` property.
+ * @return {object}
+ */
+function paramProp(vget, vset) {
+    return {
+        enumerable: true,
+        value: paramObject(vget, vset),
+    }
 }
